b9ba57ca9d1189eef51d18639a697480662147e9
Arun R S <arunvishnu40@gmail.com>
2022-01-23 00:59:16 +0100

    my changes

--- user_register_app/src/views/Register.vue
+++ user_register_app/src/views/Register.vue
@@ -19,105 +19,117 @@
               <div class="col-xl-6">
                 <div class="card-body p-md-5 text-black">
                   <h3 class="mb-5 text-uppercase">Registration form</h3>
-
-                  <div class="row">
-                    <div class="col-md-6 mb-4">
-                      <div class="form-outline">
-                        <input v-model="firstName"
-                          placeholder="First Name"
-                          type="text"
-                          id="form3Example1m"
-                          class="form-control form-control-lg"
-                        />
+                  <form @submit.prevent="onCreatePost">
+                    <div class="row">
+                      <div class="col-md-6 mb-4">
+                        <div class="form-outline">
+                          <input
+                            v-model="firstName"
+                            placeholder="First Name"
+                            type="text"
+                            id="form3Example1m"
+                            class="form-control form-control-lg"
+                          />
+                        </div>
+                      </div>
+                      <div class="col-md-6 mb-4">
+                        <div class="form-outline">
+                          <input
+                            v-model="lastName"
+                            placeholder="Last Name"
+                            type="text"
+                            id="form3Example1n"
+                            class="form-control form-control-lg"
+                          />
+                        </div>
                       </div>
                     </div>
-                    <div class="col-md-6 mb-4">
-                      <div class="form-outline">
-                        <input v-model="lastName"
-                          placeholder="Last Name"
-                          type="text"
-                          id="form3Example1n"
-                          class="form-control form-control-lg"
-                        />
-                      </div>
+
+                    <div class="row"></div>
+
+                    <div class="form-outline mb-4">
+                      <input
+                        v-model="address"
+                        placeholder="Address"
+                        type="text"
+                        id="form3Example8"
+                        class="form-control form-control-lg"
+                      />
                     </div>
-                  </div>
 
-                  <div class="row"></div>
+                    <div
+                      class="
+                        d-md-flex
+                        justify-content-start
+                        align-items-center
+                        mb-4
+                        py-2
+                      "
+                    >
+                      <select @change="onChange($event)" class="form-select">
+                        <option value="Male">Male</option>
+                        <option value="Female">Female</option>
+                        <option value="Other">Other</option>
+                      </select>
+                    </div>
 
-                  <div class="form-outline mb-4">
-                    <input v-model="address"
-                      placeholder="Address"
-                      type="text"
-                      id="form3Example8"
-                      class="form-control form-control-lg"
-                    />
-                  </div>
+                    <div class="row"></div>
 
-                  <div
-                    class="
-                      d-md-flex
-                      justify-content-start
-                      align-items-center
-                      mb-4
-                      py-2
-                    "
-                  >
-                    <select class="form-select">
-                      <option>Male</option>
-                      <option>Female</option>
-                      <option>Other</option>
-                    </select>
-                  </div>
+                    <div class="form-outline mb-4">
+                      <input v-model="DOB"
+                        placeholder="DOB"
+                        type="text"
+                        id="form3Example9"
+                        class="form-control form-control-lg"
+                      />
+                    </div>
+                    <!--
+                    <div class="mb-3">
+                      <input
+                        class="form-control"
+                        type="file"
+                        accept="image/*"
+                        id="formFile"
+                        @change="updatePhoto($event.target.name, $event.target.files)"
+                      />
+                    </div>
+                    -->
+                    <div class="form-outline mb-4">
+                      <input v-model="email"
+                        placeholder="Email"
+                        type="text"
+                        id="form3Example90"
+                        class="form-control form-control-lg"
+                      />
+                    </div>
 
-                  <div class="row"></div>
+                    <div class="form-outline mb-4">
+                      <input v-model="password"
+                        placeholder="Password"
+                        type="text"
+                        id="form3Example99"
+                        class="form-control form-control-lg"
+                      />
+                    </div>
 
-                  <div class="form-outline mb-4">
-                    <input
-                      placeholder="DOB"
-                      type="text"
-                      id="form3Example9"
-                      class="form-control form-control-lg"
-                    />
-                  </div>
-                  <div class="mb-3">
-                    <input class="form-control" type="file" id="formFile" />
-                  </div>
-                  <div class="form-outline mb-4">
-                    <input
-                      placeholder="Email"
-                      type="text"
-                      id="form3Example90"
-                      class="form-control form-control-lg"
-                    />
-                  </div>
+                    <div class="form-outline mb-4">
+                      <input v-model="password"
+                        placeholder="Repeat Password"
+                        type="text"
+                        id="form3Example97"
+                        class="form-control form-control-lg"
+                      />
+                    </div>
 
-                  <div class="form-outline mb-4">
-                    <input
-                      placeholder="Password"
-                      type="text"
-                      id="form3Example99"
-                      class="form-control form-control-lg"
-                    />
-                  </div>
-
-                  <div class="form-outline mb-4">
-                    <input
-                      placeholder="Repeat Password"
-                      type="text"
-                      id="form3Example97"
-                      class="form-control form-control-lg"
-                    />
-                  </div>
-
-                  <div class="d-flex justify-content-end pt-3">
-                    <button type="button" class="btn btn-light btn-lg">
-                      Reset all
-                    </button>
-                    <button type="button" class="btn btn-warning btn-lg ms-2">
-                      Submit form
-                    </button>
-                  </div>
+                    <div class="d-flex justify-content-end pt-3">
+                      <button type="button" class="btn btn-light btn-lg">
+                        Reset all
+                      </button>
+                      <button type="submit" class="btn btn-warning btn-lg ms-2">
+                        Submit form
+                      </button>
+                    </div>
+                  </form>
                 </div>
               </div>
             </div>
@@ -129,31 +141,59 @@
 </template>
 
 <script>
-
+import axios from "axios";
 export default {
-  name: "form",
   data() {
     return {
-      firstName:'',
-      lastName:'',
-      address:'',
-      gender:'',
-      DOB:'',
-      picture:'',
-      email:'',
-      password:''
-    
-
+      firstName: "",
+      lastName: "",
+      address: "",
+      gender: "",
+      DOB: "",
+      picture: {},
+      email: "",
+      password: "",
     };
   },
   methods: {
-   
+    onChange(event) {
+      this.gender = event.target.value;
+      console.log(this.gender);
+    },
+    updatePhoto(files) {
+      if (!files.length) return;
+
+      // Store the file data
+      this.photo = {
+        name: files[0].name,
+        data: files[0],
+      };
+    },
+    onCreatePost() {
+      axios
+        .post(
+          "https://userregistrationsystem-default-rtdb.firebaseio.com/posts.json",
+          {
+            firstName: this.firstName,
+            lastName: this.lastName,
+            address: this.address,
+            gender: this.gender,
+            picture: this.picture,
+            DOB:this.DOB,
+            email:this.email,
+            password:this.password
+
+          }
+        )
+        .then((response) => {
+          console.log(response);
+        });
+    },
   },
 };
 </script>
 
 <style scoped>
-
 .card-registration {
   font-size: 1rem;
   line-height: 2.15;
