2aef399feda7aaeddd643d4ac025fdc83dfeb98a
Arun R S <arunvishnu40@gmail.com>
2022-01-24 17:15:35 +0100

    styling of userlist table

--- user_register_app/src/components/user_comp.vue
+++ user_register_app/src/components/user_comp.vue
@@ -1,26 +1,29 @@
 <template>
-    <div>  
- <div class="container-xl">
-    <div class="table-responsive">
-      <div class="table-wrapper">
-        <table class="table table-striped table-hover">
-          <tbody>
-            <tr>
-              <td>{{data.firstName}} {{data.lastName}}</td>
-              <td>{{data.email}}</td>
-              <td>{{data.address}}</td>
-              <td>{{data.DOB}}</td>
-              <td>
+  <div>
+    <div class="container-xl">
+      <div class="table-responsive">
+        <div class="table-wrapper">
+          <table class="table table-striped">
+            <tbody>
+              <tr>
+                <td>{{ data.firstName }} {{ data.lastName }}</td>
+                <td>{{ data.email }}</td>
+                <td>{{ data.address }}</td>
+                <td>{{ data.DOB }}</td>
+                <td>
+
                 <span>
                   <a href="#editEmployeeModal" class="edit" data-toggle="modal"
-                    ><i
+                    >
+                   
+                    <i
                       class="material-icons"
                       data-toggle="tooltip"
                       title="Edit"
-                      >Edit</i
+                      >E</i
                     ></a
                   >
-                </span>
+                </span>    
                 <span>
                   <a
                     href="#deleteEmployeeModal"
@@ -30,167 +33,186 @@
                       class="material-icons"
                       data-toggle="tooltip"
                       title="Delete"
-                      >Delete</i
+                      >D</i
                     ></a
                   >
                 </span>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
-  </div>
-  
-  <div id="addEmployeeModal" class="modal fade">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <form>
-          <div class="modal-header">
-            <h4 class="modal-title">Add Employee</h4>
-            <button
-              type="button"
-              class="close"
-              data-dismiss="modal"
-              aria-hidden="true"
-            >
-              &times;
-            </button>
-          </div>
-          <div class="modal-body">
-            <div class="form-group">
-              <label>Name</label>
-              <input type="text" class="form-control" required />
-            </div>
-            <div class="form-group">
-              <label>Email</label>
-              <input type="email" class="form-control" required />
-            </div>
-            <div class="form-group">
-              <label>Address</label>
-              <textarea class="form-control" required></textarea>
-            </div>
-            <div class="form-group">
-              <label>DOB</label>
-              <input type="text" class="form-control" required />
-            </div>
-          </div>
-          <div class="modal-footer">
-            <input
-              type="button"
-              class="btn btn-default"
-              data-dismiss="modal"
-              value="Cancel"
-            />
-            <input type="submit" class="btn btn-success" value="Add" />
-          </div>
-        </form>
-      </div>
-    </div>
-  </div>
-  <div id="editEmployeeModal" class="modal fade">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <form>
-          <div class="modal-header">
-            <h4 class="modal-title">Edit Employee</h4>
-            <button
-              type="button"
-              class="close"
-              data-dismiss="modal"
-              aria-hidden="true"
-            >
-              &times;
-            </button>
-          </div>
-          <div class="modal-body">
-            <div class="form-group">
-              <label>Name</label>
-              <input type="text" class="form-control" required />
-            </div>
-            <div class="form-group">
-              <label>Email</label>
-              <input type="email" class="form-control" required />
-            </div>
-            <div class="form-group">
-              <label>Address</label>
-              <textarea class="form-control" required></textarea>
-            </div>
-            <div class="form-group">
-              <label>DOB</label>
-              <input type="text" class="form-control" required />
-            </div>
-          </div>
-          <div class="modal-footer">
-            <input
-              type="button"
-              class="btn btn-default"
-              data-dismiss="modal"
-              value="Cancel"
-            />
-            <input type="submit" class="btn btn-info" value="Save" />
-          </div>
-        </form>
-      </div>
-    </div>
-  </div>
 
-  <div id="deleteEmployeeModal" class="modal fade">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <form>
-          <div class="modal-header">
-            <h4 class="modal-title">Delete Employee</h4>
-            <button
-              type="button"
-              class="close"
-              data-dismiss="modal"
-              aria-hidden="true"
-            >
-              &times;
-            </button>
-          </div>
-          <div class="modal-body">
-            <p>Are you sure you want to delete these Records?</p>
-            <p class="text-warning">
-              <small>This action cannot be undone.</small>
-            </p>
-          </div>
-          <div class="modal-footer">
-            <input
-              type="button"
-              class="btn btn-default"
-              data-dismiss="modal"
-              value="Cancel"
-            />
-            <input type="submit" class="btn btn-danger" value="Delete" />
-          </div>
-        </form>
+    <div id="addEmployeeModal" class="modal fade">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <form>
+            <div class="modal-header">
+              <h4 class="modal-title">Add Employee</h4>
+              <button
+                type="button"
+                class="close"
+                data-dismiss="modal"
+                aria-hidden="true"
+              >
+                &times;
+              </button>
+            </div>
+            <div class="modal-body">
+              <div class="form-group">
+                <label>Name</label>
+                <input type="text" class="form-control" required />
+              </div>
+              <div class="form-group">
+                <label>Email</label>
+                <input type="email" class="form-control" required />
+              </div>
+              <div class="form-group">
+                <label>Address</label>
+                <textarea class="form-control" required></textarea>
+              </div>
+              <div class="form-group">
+                <label>DOB</label>
+                <input type="text" class="form-control" required />
+              </div>
+            </div>
+            <div class="modal-footer">
+              <input
+                type="button"
+                class="btn btn-default"
+                data-dismiss="modal"
+                value="Cancel"
+              />
+              <input type="submit" class="btn btn-success" value="Add" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+    <div id="editEmployeeModal" class="modal fade">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <form>
+            <div class="modal-header">
+              <h4 class="modal-title">Edit Employee</h4>
+              <button
+                type="button"
+                class="close"
+                data-dismiss="modal"
+                aria-hidden="true"
+              >
+                &times;
+              </button>
+            </div>
+            <div class="modal-body">
+              <div class="form-group">
+                <label>Name</label>
+                <input type="text" class="form-control" required />
+              </div>
+              <div class="form-group">
+                <label>Email</label>
+                <input type="email" class="form-control" required />
+              </div>
+              <div class="form-group">
+                <label>Address</label>
+                <textarea class="form-control" required></textarea>
+              </div>
+              <div class="form-group">
+                <label>DOB</label>
+                <input type="text" class="form-control" required />
+              </div>
+            </div>
+            <div class="modal-footer">
+              <input
+                type="button"
+                class="btn btn-default"
+                data-dismiss="modal"
+                value="Cancel"
+              />
+              <input type="submit" class="btn btn-info" value="Save" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+
+    <div id="deleteEmployeeModal" class="modal fade">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <form>
+            <div class="modal-header">
+              <h4 class="modal-title">Delete Employee</h4>
+              <button
+                type="button"
+                class="close"
+                data-dismiss="modal"
+                aria-hidden="true"
+              >
+                &times;
+              </button>
+            </div>
+            <div class="modal-body">
+              <p>Are you sure you want to delete these Records?</p>
+              <p class="text-warning">
+                <small>This action cannot be undone.</small>
+              </p>
+            </div>
+            <div class="modal-footer">
+              <input
+                type="button"
+                class="btn btn-default"
+                data-dismiss="modal"
+                value="Cancel"
+              />
+              <input type="submit" class="btn btn-danger" value="Delete" />
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   </div>
-</div> 
 </template>
 
 <script>
-export default{
-    name:'usercomp',
-   props: {
-    data: Object
-   },
+export default {
+  name: "usercomp",
+  props: {
+    data: Object,
+  },
   computed: {
-    gt(){
-    console.log("key",this.key)
-    return this.key
-    }
-  }
-
-}
-
+    gt() {
+      console.log("key", this.key);
+      return this.key;
+    },
+  },
+};
 </script>
 
 <style scoped>
-span{
-padding-left: 15px;
+span {
+  padding-left: 50px;
+}
+table,
+th,
+td {
+  border: 1px solid black;
+  table-layout: fixed;
+  border-collapse: collapse;
+  margin: 25px 0;
+  font-size: 0.9em;
+  font-family: sans-serif;
+  min-width: 400px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+  
+}
+
+td {
+  width: 25%;
+}
+.edit
+{
+  font-size: 1px;
 }
 </style>
